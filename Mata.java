0db33c6187c33d2c2a2497af70cc82566a697aa7
onyxpray <76498870+onyxpray@users.noreply.github.com>
2021-07-04 22:35:01 +0700

    Update Mata.java

--- Mata.java
+++ Mata.java
@@ -1,4 +1,4 @@
-public class Moduls{
+public class Mata{
   public static void main(String[] args) {
     System.out.println("Ini adalah modul MATA (Manajemen Pengetahuan) yang berfungsi sebagai berfungsi sebagai platform kolaborasi berbasis media sosial");
   }
